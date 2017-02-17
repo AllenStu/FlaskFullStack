7a0f2fb049a02403c126b71de1fc5a973bd05650
AllenStu <arthurallenbalang@gmail.com>
2017-02-17 17:11:11 +0800

    NDB template

--- TimeTracker/TimeTracker/home/__init__.py
+++ TimeTracker/TimeTracker/home/__init__.py
@@ -10,17 +10,17 @@ def add_routes(app=None):
 
     @blueprint_home.route('/home', methods=['GET'])
     def get():
-
-
         # account = jsonify(dict(AccountService.create(account2)))
         account = AccountService().create()
         my_key = str(account.key.urlsafe())
-        # account_type = type(account)
         return render_template('home.html', account=my_key)
-        # return redirect('/')
+
 
     @blueprint_home.route('/home', methods=['POST'])
     def post():
+        account = AccountService().create()
+        my_key = str(account.key.urlsafe())
         return render_template('home.post.html')
+
 
     app.register_blueprint(blueprint_home)
